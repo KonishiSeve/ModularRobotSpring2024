912cc9364f4f0e26b7ceee87cce13150a3bc20f7
Séverin Konishi <konishi.seve@gmail.com>
2024-06-06 11:59:57 +0200

    Template cleanup

--- ModRobClient/ModRobClient.py
+++ ModRobClient/ModRobClient.py
@@ -1,5 +1,6 @@
 import socket
 
+#create an object of this class and use the methods to interact with the robot
 class ModRob:
     def __init__(self, ip, module_udp_port, timeout_milliseconds):
         #save the ip address except the last number
@@ -10,8 +11,17 @@ class ModRob:
         #configuring the socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.settimeout(timeout_milliseconds/1000.0)
+        #enable broadcasting
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
+
+    #Calls a module discovery on the targeted module
+    #returns a a list of dictionaries (one for each device) with format:
+    #   {"id": id of device (start from 1) ,
+    #    "command_size": number of command bytes for device,
+    #    "data_size": number of data bytes for device,
+    #    "device_attributes": a bytearray of the attributes defined in the firmware for this device}
+
     def module_discovery(self, module_id):
         #send packet that corresponds to module discovery (which is [0x00, 0x01])
         self.socket.sendto(b'\x00\x01', ("{0}.{1}".format(self.ip_base, module_id), self.module_udp_port))
@@ -24,6 +34,7 @@ class ModRob:
             return None
         return self.parse_model_discovery(server[0].split(".")[-1], data)
     
+    #should not be called by user, parses the raw UDP packet
     def parse_model_discovery(self, module_id, data):
         device_list = []
         index = 0
@@ -38,6 +49,13 @@ class ModRob:
             index += device_length + 1
         return {"module_id":int(module_id) , "devices":device_list}
 
+    #Calls a structure discovery on the whole robot
+    #returns a a list of dictionaries (one for each module) with format:
+    #   {"module_id": id of module (last number of IP) ,
+    #    "ports_states": list of module ids (length is equal to the number of ports, id is zero if nothing is connected to this port),
+    #    "ports_attributes": list of bytearrays (one bytearray for each port),
+    #    "module_attributes": a bytearray of the module attributes defined in the firmware}
+
     def structure_discovery(self, timeout=1):
         #send packet that corresponds to structure discovery ([0x00,0x00]) via the broadcast address (255)
         self.socket.sendto(b'\x00\x00', ("{0}.{1}".format(self.ip_base, 255), self.module_udp_port))
@@ -51,6 +69,7 @@ class ModRob:
                 return module_list
         return module_list
     
+    #should not be called by user, parses the raw UDP packet
     def parse_structure_discovery(self, module_id, data):
         #extract the ports number and the neighbors
         data_index = 0
@@ -75,13 +94,15 @@ class ModRob:
 
         return {"module_id": int(module_id), "ports_states": neighbours, "ports_attributes": ports_attributes, "module_attributes": module_attributes}
 
+    #write a bytearray to a target device in a target module, it will be passed to the command function handle defined in the firmware
     def write_command(self, target_module, target_device, command):
-        byte0 = (1<<7) + (target_device&(0b01111111))
+        byte0 = (1<<7) + (target_device&(0b01111111)) #put the first bit to 1 to indicate a command write packet
         payload = bytearray([byte0]) + command
         self.socket.sendto(payload, ("{0}.{1}".format(self.ip_base, target_module), self.module_udp_port))
 
+    #read a bytearray from a target device in a target module, the data function handle defined in the firmware will be ran to find this bytearray
     def read_data(self, target_module, target_device):
-        byte0 = target_device&(0b01111111)
+        byte0 = target_device&(0b01111111) #put the first bit to 0 to indicate a data read packet
         self.socket.sendto(bytearray([byte0]), ("{0}.{1}".format(self.ip_base, target_module), self.module_udp_port))
         try:
             data, server = self.socket.recvfrom(1024)
--- ModRobESP32/include/modrob.h
+++ ModRobESP32/include/modrob.h
@@ -28,7 +28,7 @@ class ModRob {
         uint8_t devices_size;
         uint8_t devices_size_max;
 
-        //functions mapped to udp commands
+        //functions mapped to udp commands, called in process_udp method
         uint16_t udp_struct_disc(uint8_t *udp_tx_buffer);
         uint16_t udp_module_disc(uint8_t *udp_tx_buffer);
         uint16_t udp_write_command(uint8_t *packet, uint8_t packet_size, uint8_t *udp_tx_buffer);
@@ -37,10 +37,15 @@ class ModRob {
     public:
     //Should be called by the user to configure the module
         bool setup(uint8_t module_id, uint8_t bytes_per_port_attribute, uint8_t max_devices_number);
+
+        //add a docking port with corresponding pin number and attributes
         bool add_port_rx(uint8_t rx_pin, uint8_t *port_attributes);
+        //set the TX pin to use for all the ports
         bool set_port_tx(uint8_t tx_pin);
 
+        //add a device to the module
         bool add_device(Device device);
+        //define the module attributes
         bool set_module_attributes(uint8_t *module_attributes, uint16_t module_attributes_size);
 
     //Called in the loop() of the main to process a UDP packet and act accordingly
